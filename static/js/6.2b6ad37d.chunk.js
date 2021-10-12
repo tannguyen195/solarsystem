77bd2edda4f8a763411d1f4395f46821fafd0427
Tan Nguyen <tannguyen@Tans-MacBook-Pro.local>
2021-10-12 23:22:27 +0700

    Updates

---
+++ static/js/6.2b6ad37d.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpsolarsystem=this.webpackJsonpsolarsystem||[]).push([[6],{70:function(n,t,e){"use strict";e.r(t);var s=e(6),c=e(9),r=Object(s.lazy)((function(){return e.e(3).then(e.bind(null,68))})),l=Object(s.lazy)((function(){return e.e(4).then(e.bind(null,67))})),u=Object(s.lazy)((function(){return e.e(5).then(e.bind(null,69))}));t.default=function(){return Object(c.jsxs)(c.Fragment,{children:[Object(c.jsx)(r,{}),Object(c.jsx)(l,{}),Object(c.jsx)(u,{position:[0,-400,0],scale:new Array(3).fill(300),count:2e3})]})}}}]);
+//# sourceMappingURL=6.2b6ad37d.chunk.js.map
